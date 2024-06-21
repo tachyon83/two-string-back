6b751bdad16674a017001333c7b87f4e55cba604
tachyon <83tachyon@gmail.com>
2024-06-21 11:36:57 +0900

    [chore] console removed

--- src/controllers/string.controller.ts
+++ src/controllers/string.controller.ts
@@ -22,10 +22,6 @@ export default class StringController extends BaseController {
   }
 
   compare = async (req: express.Request, res: express.Response) => {
-    console.log(25, req.body.str1);
-    console.log(26, req.body.str2);
-    console.log();
-
     return this.service.compare(req.body.str1, req.body.str2);
   };
 }
--- src/services/string.service.ts
+++ src/services/string.service.ts
@@ -5,11 +5,7 @@ import {
 
 export default class StringService {
   compare(str1: string, str2: string): string {
-    const cst = commonSequenceTableMaker(str1, str2);
-    console.log(6, cst);
-    console.log();
-
-    const res = commonSequenceIndexFinder(cst);
+    const res = commonSequenceIndexFinder(commonSequenceTableMaker(str1, str2));
     console.log(9, res.str1arr);
     console.log(10, res.str2arr);
     console.log();
