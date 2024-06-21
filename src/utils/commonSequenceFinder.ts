de0e9d9d980bd0d1002cd9a7ec4e741a46eda064
tachyon <83tachyon@gmail.com>
2024-06-21 17:46:53 +0900

    [fix] LCS fixed

--- src/utils/commonSequenceFinder.ts
+++ src/utils/commonSequenceFinder.ts
@@ -1,3 +1,5 @@
+import { StringCompareOutput } from "./dto";
+
 export function commonSequenceTableMaker(
   str1: string,
   str2: string
@@ -12,6 +14,11 @@ export function commonSequenceTableMaker(
     for (let j = 0; j < str2len; ++j) {
       if (str1[i] === str2[j]) {
         cst2dArr[i + 1][j + 1] = cst2dArr[i][j] + 1;
+      } else {
+        cst2dArr[i + 1][j + 1] = Math.max(
+          cst2dArr[i][j + 1],
+          cst2dArr[i + 1][j]
+        );
       }
     }
   }
@@ -19,29 +26,34 @@ export function commonSequenceTableMaker(
   return cst2dArr;
 }
 
-export function commonSequenceIndexFinder(cst2dArr: number[][]): {
-  str1arr: number[];
-  str2arr: number[];
-} {
-  const str1arr = [];
-  const str2arr = [];
+export function commonSequenceIndexFinder(
+  cst2dArr: number[][],
+  str1: string,
+  str2: string
+): StringCompareOutput {
+  const str1CommonIdxArr = [];
+  const str2CommonIdxArr = [];
 
-  const str1len = cst2dArr.length;
-  const str2len = cst2dArr[0].length;
+  // console.log(30, cst2dArr);
 
-  let k = 1;
+  let str1idx = str1.length - 1;
+  let str2idx = str2.length - 1;
 
-  for (let i = 1; i <= str1len; ++i) {
-    for (let j = k; j <= str2len; ++j) {
-      if (cst2dArr[i][j] > 0) {
-        str1arr.push(i - 1);
-        str2arr.push(j - 1);
-        k++;
-        break;
-      }
+  while (str1idx >= 0 && str2idx >= 0) {
+    if (str1[str1idx] === str2[str2idx]) {
+      str1CommonIdxArr.push(str1idx--);
+      str2CommonIdxArr.push(str2idx--);
+    } else if (
+      cst2dArr[str1idx][str2idx + 1] >= cst2dArr[str1idx + 1][str2idx]
+    ) {
+      str1idx--;
+    } else {
+      str2idx--;
     }
-    if (k > str2len) break;
   }
 
-  return { str1arr, str2arr };
+  str1CommonIdxArr.sort((a, b) => a - b);
+  str2CommonIdxArr.sort((a, b) => a - b);
+
+  return { str1CommonIdxArr, str2CommonIdxArr };
 }
